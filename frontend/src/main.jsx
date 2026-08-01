8e705e4344d9d57887ca6f388d8643f950dd7052
fabienkazak-maker <fabien.kazak@gmail.com>
2026-08-01 10:06:47 +0200

    Enable readable API errors for issue #12

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
+import "./api-error-messages.js";
 import "./climbcrew-enhancements.js";
 import "./issue-11.css";
 
