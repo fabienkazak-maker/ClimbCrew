6a124bd46439f582745d797d204f9d8f7287e259
fabienkazak-maker <fabien.kazak@gmail.com>
2026-08-01 16:45:03 +0200

    feat(frontend): enable administrator user management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,12 @@ import App from "./App.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 import "./api-error-messages.js";
 import "./issue-13-access-page.js";
+import "./admin-user-management.js";
 import "./climbcrew-enhancements.js";
 import "./issue-11.css";
 import "./issue-13-access-page.css";
 import "./issue-16-routes.css";
+import "./admin-user-management.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
