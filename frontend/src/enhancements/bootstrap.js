b70d6a9e9b3fe14463177a01fd24dc916793e039
thithipetit-hash <thithi.petit@gmail.com>
2026-08-07 14:24:35 +0200

    sauvegarde avant refactor UI

---
+++ frontend/src/enhancements/bootstrap.js
@@ -0,0 +1,4 @@
+import "../api-error-messages.js";
+import "../issue-13-access-page.js";
+import "../admin-user-management.js";
+import "../climbcrew-enhancements.js";
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,3 +18,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ErrorBoundary>
   </React.StrictMode>
 );
+
+
