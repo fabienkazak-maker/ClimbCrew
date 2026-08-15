a14208ebc4227526961b7979620152d54d9a0058
thithipetit-hash <thithipetit@gmail.com>
2026-08-15 21:01:25 +0200

    Ajouter les 8 évolutions individuelles du Paresseux

--- frontend/src/components/ProfileGecko.jsx
+++ frontend/src/components/ProfileGecko.jsx
@@ -22,7 +22,7 @@ export const AVATAR_OPTIONS = Object.freeze([
   { id: "bouquetin", label: "Bouquetin", group: "Animaux", image: asset(AVATAR_ROOT, "bouquetin"), evolutionImages: evolutionImages("bouquetin") },
   { id: "capucin", label: "Singe capucin", group: "Animaux", image: asset(AVATAR_ROOT, "capucin"), evolutionImages: evolutionImages("capucin") },
   { id: "ecureuil", label: "Écureuil", group: "Animaux", image: asset(AVATAR_ROOT, "ecureuil"), evolutionImages: evolutionImages("ecureuil") },
-  { id: "paresseux", label: "Paresseux", group: "Animaux", image: asset(AVATAR_ROOT, "paresseux") },
+  { id: "paresseux", label: "Paresseux", group: "Animaux", image: asset(AVATAR_ROOT, "paresseux"), evolutionImages: evolutionImages("paresseux") },
   { id: "leopard_neiges", label: "Léopard des neiges", group: "Animaux", image: asset(AVATAR_ROOT, "leopard-neiges") },
   { id: "orang_outan", label: "Orang-outan bloqueur", group: "Animaux", image: asset(AVATAR_ROOT, "orang-outan") },
   { id: "pieuvre", label: "Pieuvre grimpeuse", group: "Animaux", image: asset(AVATAR_ROOT, "pieuvre") },
