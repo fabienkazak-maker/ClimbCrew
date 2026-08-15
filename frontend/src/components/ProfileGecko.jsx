63f49a6bc751da6ecfdef25c995f617c43c20581
thithipetit-hash <thithipetit@gmail.com>
2026-08-15 22:12:04 +0200

    Ajouter les 8 évolutions individuelles de la Pieuvre

--- frontend/src/components/ProfileGecko.jsx
+++ frontend/src/components/ProfileGecko.jsx
@@ -25,7 +25,7 @@ export const AVATAR_OPTIONS = Object.freeze([
   { id: "paresseux", label: "Paresseux", group: "Animaux", image: asset(AVATAR_ROOT, "paresseux"), evolutionImages: evolutionImages("paresseux") },
   { id: "leopard_neiges", label: "Léopard des neiges", group: "Animaux", image: asset(AVATAR_ROOT, "leopard-neiges"), evolutionImages: evolutionImages("leopard_neiges") },
   { id: "orang_outan", label: "Orang-outan bloqueur", group: "Animaux", image: asset(AVATAR_ROOT, "orang-outan"), evolutionImages: evolutionImages("orang_outan") },
-  { id: "pieuvre", label: "Pieuvre grimpeuse", group: "Animaux", image: asset(AVATAR_ROOT, "pieuvre") },
+  { id: "pieuvre", label: "Pieuvre grimpeuse", group: "Animaux", image: asset(AVATAR_ROOT, "pieuvre"), evolutionImages: evolutionImages("pieuvre") },
   { id: "robot", label: "Robot assureur", group: "Personnages", image: asset(AVATAR_ROOT, "robot") },
   { id: "astronaute", label: "Astronaute en SAE", group: "Personnages", image: asset(AVATAR_ROOT, "astronaute") },
   { id: "capybara", label: "Capybara zen", group: "Animaux", image: asset(AVATAR_ROOT, "capybara") },
