85897b83d314edd93f3f0d8fc7882b95634640ee
thithipetit-hash <thithipetit@gmail.com>
2026-08-16 16:49:45 +0200

    Ajouter les 8 évolutions individuelles du Chevalier

--- frontend/src/components/ProfileGecko.jsx
+++ frontend/src/components/ProfileGecko.jsx
@@ -29,7 +29,7 @@ export const AVATAR_OPTIONS = Object.freeze([
   { id: "robot", label: "Robot assureur", group: "Personnages", image: asset(AVATAR_ROOT, "robot"), evolutionImages: evolutionImages("robot") },
   { id: "astronaute", label: "Astronaute en SAE", group: "Personnages", image: asset(AVATAR_ROOT, "astronaute"), evolutionImages: evolutionImages("astronaute") },
   { id: "capybara", label: "Capybara zen", group: "Animaux", image: asset(AVATAR_ROOT, "capybara"), evolutionImages: evolutionImages("capybara") },
-  { id: "chevalier", label: "Chevalier grimpeur", group: "Personnages", image: asset(AVATAR_ROOT, "chevalier") },
+  { id: "chevalier", label: "Chevalier grimpeur", group: "Personnages", image: asset(AVATAR_ROOT, "chevalier"), evolutionImages: evolutionImages("chevalier") },
   { id: "humain_homme", label: "Grimpeur hyperréaliste", group: "Personnages", image: asset(AVATAR_ROOT, "humain-homme") },
   { id: "humain_femme", label: "Grimpeuse hyperréaliste", group: "Personnages", image: asset(AVATAR_ROOT, "humain-femme") },
   { id: "fraise", label: "Fraise verticale", group: "Fruits", image: asset(PROFILE_ROOT, "avatar-fraise") },
